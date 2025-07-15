0c8ed6efd073ba0f31cbd3ccd4af9547d163cd9e
luckeepanda <90700406+luckeepanda@users.noreply.github.com>
2025-07-14 23:12:06 -0400

    Updated Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -128,7 +128,7 @@ const Login: React.FC = () => {
             </div>
 
             <div>
-              <label className="block text-sm font-medium text-white-100 mb-2">
+              <label className="block text-sm font-medium text-steel-blue-100 mb-2">
                 Password
               </label>
               <div className="relative text-steel-blue">
