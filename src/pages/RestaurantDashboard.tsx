c4915740b30b8a56366e98c2f18cea10c9d9271a
luckeepanda <90700406+luckeepanda@users.noreply.github.com>
2025-07-18 22:46:39 -0400

    Updated RestaurantDashboard.tsx

--- src/pages/RestaurantDashboard.tsx
+++ src/pages/RestaurantDashboard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import React, { useState, useEffect } from 'react';
+// import React from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import Header from '../components/Header';
