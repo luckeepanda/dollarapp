32e2404d3cedd4e13d26d18d4703cc4c93d27419
luckeepanda <90700406+luckeepanda@users.noreply.github.com>
2025-09-09 11:40:34 -0400

    Fix undefined ModernBusinessCard reference

--- src/components/ModernGameCard.tsx
+++ src/components/ModernGameCard.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { getRandomEmojiFromCategory, getRandomAnimationClass } from '../utils/emojiSystem';
-import { Trophy, Users, DollarSign, Star, Play, Clock, Target } from 'lucide-react';
 
 interface Game {
   id: string;
