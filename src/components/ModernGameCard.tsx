197fc048107030168bd6e6bd5451e32fe2e7c7ff
luckeepanda <90700406+luckeepanda@users.noreply.github.com>
2025-09-15 15:32:42 -0400

    Fix ModernBusinessCard undefined error

--- src/components/ModernGameCard.tsx
+++ src/components/ModernGameCard.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getRandomEmojiFromCategory, getRandomAnimationClass } from '../utils/emojiSystem';
+import { Play, ArrowRight, DollarSign } from 'lucide-react';
 
 interface Game {
   id: string;
