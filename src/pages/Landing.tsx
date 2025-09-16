b20147e1a5285df80153ecfbe914db3defd9edb3
luckeepanda <90700406+luckeepanda@users.noreply.github.com>
2025-09-16 00:20:27 -0400

    Updated Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -315,7 +315,7 @@ const Landing: React.FC = () => {
               <h1 className="text-6xl sm:text-7xl font-bold font-display mb-3">
                 <span className="bg-gradient-to-r from-green-500 to-green-600 bg-clip-text text-transparent">Dollar App</span>
               </h1>
-              <p className="text-xl sm:text-2xl font-medium text-primary-600 font-display">
+              <p className="text-xl sm:text-2xl font-medium text-primary-600 font-display font-bold">
                 $1 games for real prizes near you.
               </p>
               
